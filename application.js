061a60f4d72fa32aa1d095cc7e36496332541bd9
Cortni <cortni.fernandez@gmail.com>
2021-02-19 12:08:13 -0500

    Dom ready response and GET response

---
+++ application.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  console.log('dom ready');
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    datatype: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+  
+});
