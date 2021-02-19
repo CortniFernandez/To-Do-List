fda41c4ae08ee88e27a66b97899064acd597e5f9
Cortni <cortni.fernandez@gmail.com>
2021-02-19 15:43:03 -0500

    Delete working

--- application.js
+++ application.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<p>' + task.content + '</p>');
-        })
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+        });
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
@@ -44,5 +44,22 @@ $(document).ready(function () {
   });
 
   getAndDisplayAllTasks();
+
+  var deleteTask = function (id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=301',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  };
+  
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
 
 });
