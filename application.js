51a5a1efcb7cf66854b5fcebac4be60fa64ad834
Cortni <cortni.fernandez@gmail.com>
2021-02-19 14:43:40 -0500

    Form working for new task

--- application.js
+++ application.js
@@ -1,16 +1,28 @@
 $(document).ready(function () {
-  console.log('dom ready');
 
-  $.ajax({
-    type: 'GET',
-    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-    datatype: 'json',
-    success: function (response, textStatus) {
-      console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=48',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });  
+  }
+
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
   });
   
 });
